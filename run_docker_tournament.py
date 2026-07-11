d6549d92689940ace26d87fee68c7c47c13fab3b
joendter <71767955+joendter@users.noreply.github.com>
2026-07-11 10:56:04 +0200

    finish docker tournament runner

---
+++ run_docker_tournament.py
@@ -0,0 +1,188 @@
+from subprocess import DEVNULL
+from typing import Tuple
+
+import os
+import time
+import subprocess
+from pathlib import Path
+import re
+import tempfile
+import configparser
+import secrets
+
+
+class Tournament:
+    N_SEEDS = 5
+    def __init__(self, ai_directory = "submissions"):
+        self.used_ports = set()
+        self.next_port = 1200
+        self.used_seeds = set()
+        self.instances : dict[tuple[str,str,str], subprocess.Popen]= {}
+        self.AIS_DIRECTORY = ai_directory
+        self.ais = self.get_files(self.AIS_DIRECTORY)
+        self.results = {
+            ai:{
+                ai2:[] for ai2 in self.ais if ai2 != ai
+            } for ai in self.ais
+        }
+        self.config_directory = tempfile.TemporaryDirectory()
+
+
+    @staticmethod
+    def get_files(dir: str) -> list[str]:
+        return [dir + "/" + p.name for p in Path(dir).iterdir() if p.is_file()]
+
+    def process_result(self, config_location, ai1, ai2):
+        process = self.instances[(config_location, ai1, ai2)]
+        print(f"Waiting for match {ai1} vs {ai2} with config {config_location}")
+        stdout, stderr = process.communicate()
+        ma = re.findall(r'\|\s*(\d+)\s+P\.:\s+ai(\d)', stdout)
+        if len(ma) < 2:
+            print(stdout)
+            print(stderr)
+            raise Exception("Unexpected docker output")
+        ais = [ai1,ai2]
+        small_results = {}
+        for (score, index) in ma:
+            me = ais[int(index)-1]
+            small_results[me] = int(score)
+        self.results[ai1][ai2].append((small_results[ai1],small_results[ai2]))
+        self.results[ai2][ai1].append((small_results[ai2],small_results[ai1]))
+
+
+
+
+    def get_and_inc_port(self) -> int:
+        retval = self.next_port
+        self.used_ports.add(self.next_port)
+        self.next_port += 1
+        if self.next_port > 64000:
+            raise OverflowError("too many ports used")
+        return retval
+
+
+    def start_once(self, config_location: str, ai1: str, ai2: str):
+        print(f"Starting match {ai1} vs {ai2} with config {config_location}")
+        env = os.environ.copy()
+        port = self.get_and_inc_port()
+        env["PORT"] = f"{port}"
+        env["CONFIG_LOCATION"] = config_location
+        env["AI1_LOCATION"] = ai1
+        env["AI2_LOCATION"] = ai2
+        self.instances[(config_location, ai1, ai2)] = (subprocess.Popen([
+        "docker", "compose",
+        "-p", self.get_project_name(port),
+        "up"
+    ], env = env,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        )
+        )
+
+    def process_results(self):
+        for k in self.instances.keys():
+            self.process_result(k[0], k[1], k[2])
+
+    def run_tournament(self):
+        self.start_all()
+        self.process_results()
+
+    def start_all(self):
+        [self.get_new_seed() for _ in range(self.N_SEEDS)]
+        for seed in self.used_seeds:
+            config = self.create_temp_config(seed)
+            ticks = self.get_ticks(config)
+            for ai1 in self.ais:
+                for ai2 in self.ais:
+                    while not self.should_spawn_another_match():
+                        time.sleep(0.1)
+                    if ai1 != ai2:
+                        self.start_once(
+                            config,
+                            ai1,
+                            ai2
+                        )
+                    time.sleep(
+                        ticks / os.cpu_count() / 500
+                    )
+
+
+    def print_results(self):
+        scores = [(ai, self.calculate_score(ai)) for ai in self.ais]
+
+        scores.sort(
+            key=lambda x: (x[1][0], x[1][1], -x[1][2]),
+            reverse=True,
+        )
+
+        for ai, score in scores:
+            print(f"{ai}: {score}")
+
+    def calculate_score(self, ai: str) -> Tuple[int,int,int]:
+        win = draw = loss = 0
+        for subresults in self.results[ai].values():
+            for my_score, other_score in subresults:
+                if my_score > other_score:
+                    win += 1
+                if my_score == other_score:
+                    draw += 1
+                if my_score < other_score:
+                    loss += 1
+        return win,draw,loss
+
+    @staticmethod
+    def should_spawn_another_match():
+        load1, load5, load15 = os.getloadavg()
+        cpus = os.cpu_count()
+        return load1 < cpus * 0.9
+
+    def cleanup(self):
+        for port in self.used_ports:
+            subprocess.run([
+                "docker", "compose",
+                "-p", self.get_project_name(port),
+                "down",
+                "--remove-orphans",
+            ],
+            stdout=DEVNULL,
+            stderr=DEVNULL)
+        self.config_directory.cleanup()
+
+    @staticmethod
+    def get_project_name(port) -> str:
+        return f"game_{port}"
+
+    def create_temp_config(self, seed: int, original_path: str = "config.ini") -> str:
+        config = configparser.ConfigParser()
+        config.read(original_path)
+
+        config["global"]["seed"] = str(seed)
+
+        output_path = Path(f"{self.config_directory.name}/config_{seed}.ini")
+
+        with output_path.open("w") as f:
+            config.write(f)
+
+
+        return str(output_path)
+
+    @staticmethod
+    def get_ticks(config_path: str) -> int:
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        return int(config["global"]["playtime"])
+
+    def get_new_seed(self):
+        seed = int.from_bytes(secrets.token_bytes(32))
+        self.used_seeds.add(seed)
+        return seed
+
+if __name__ == "__main__":
+    t = Tournament("submissions2")
+    try:
+        t.run_tournament()
+        t.print_results()
+    finally:
+        t.cleanup()
+    #print(t.results)
